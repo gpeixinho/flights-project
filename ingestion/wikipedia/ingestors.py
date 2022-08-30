c3d69a23a8b16d698fb9feb8eb3399b4f87dc363
Gustavo Peixinho <gustavoglpeixinho@gmail.com>
2022-08-30 18:58:12 -0300

    Implement Wikipedia airport code ingestor

---
+++ ingestion/wikipedia/ingestors.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import List
+from ingestion.wikipedia.scrapers import AirportCodeScraper
+
+class DataIngestor(ABC):
+    def __init__(self, writer) -> None:
+        self.writer = writer
+
+    @abstractmethod
+    def ingest(self, **kwargs):
+        pass
+
+class AirportCodeIngestor(DataIngestor):
+    def __init__(self, writer, letters: List):
+        super().__init__(writer)
+        self.letters = letters
+    
+    def ingest(self):
+        for letter in self.letters:
+            scraper = AirportCodeScraper(letter=letter)
+            data = scraper.get_data()
+            self.writer(path=f"{scraper.path}/{scraper.letter}").write(data)
